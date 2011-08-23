07c2a68582641a120cc85d961d866bf7f79e2936
Michael Ambrus <michael.ambrus@sonyericsson.com>
2011-08-23 16:42:26 +0200

    Added ability to src.mgrep.sh in SCons based project as well
    Good for AMSS build system grepping.
    
    Used withouth prejudice in either make or Scons projects.

--- mgrep.sh
+++ mgrep.sh
@@ -7,9 +7,10 @@ if [ -z $MGREP_SH ]; then
 MGREP_SH="mgrep.sh"
 
 # Atempts to work as the very useful Android mgrep utility
-
+# Note that for Scons dependency files (SConscript, SConstruct, *.min), I don't know where the convention of
+# putting these dependencies into *.min comes from. It might not be standard.
 function mgrep() {
-	find . -iregex '\(.*\.mk$\|.*makefile.*$\)' -exec egrep "$1" -nH --color=always '{}' ';' \
+	find . -iregex '\(.*\.mk$\|.*makefile.*$\|.*SConscript$\|.*SConstruct$\|.*\.min$\)' -exec egrep "$1" -nH --color=always '{}' ';' \
 	| grcat conf.gcc
 }
 
