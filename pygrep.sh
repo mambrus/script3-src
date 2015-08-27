37aa4958ab3e6451b62a7cd76ff5a4a3162babc4
michael.ambrus <ambrmi09@gmail.com>
2015-08-27 19:00:34 +0200

    [?grep]: pygrep.sh added
    Change-Id: I6516bc021721e5ff8035b441bf41f965df59296d

---
+++ pygrep.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Author: Michael Ambrus (ambrmi09@gmail.com)
+# 2011-09-27
+
+if [ -z $PYGREP_SH ]; then
+
+PYGREP_SH="pygrep.sh"
+
+# Attempts to work as the very useful Android cgrep utility
+
+XGREP_PATTERN='\(.*\.py$\)'
+
+source s3.ebasename.sh
+source src.xgrep.sh
+if [ "$PYGREP_SH" == $( ebasename $0 ) ]; then
+	#Not sourced, do something with this.
+
+	XGREP_SH_INFO=${PYGREP_SH}
+	XGREP_IGNORE=${XGREP_IGNORE-'
+		-path ./out* -prune -o
+		-path ./.repo/ -prune -o
+		-path "*/.git/" -prune -o '}
+	source .src.ui..xgrep.sh
+
+	xgrep "$@" "${COLOR_PARAM}"
+
+	exit $?
+else
+	echo "Don't source $XGREP_SH_INFO"
+fi
+
+fi
